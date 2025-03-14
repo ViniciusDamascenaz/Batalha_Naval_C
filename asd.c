0785340ae2f64d817af1dc8dd79ec4f5421cb065
Vinicius Damascena <85317029+ViniciusDamascenaz@users.noreply.github.com>
2025-03-14 00:11:53 +0000

    desafio aventureiro concluido

---
+++ asd.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'};
+
+    printf("%d", letras['A']);  
+
+}
--- batalha_naval.c
+++ batalha_naval.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
 
-    printf("-- B A T A L H A -- N A V A L --\n");
-
-    int tabuleiro[10][10]; // Setando matriz do tabuleiro
-    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'}; // Criando lista de letras
-    for(int i = 0; i<10; i++){
-        for(int j = 0; j<10; j++){
-            tabuleiro[i][j] = 0; // Setando todos os valores da matriz para zero
-
-        }
-    }
-
-    tabuleiro[2][3] = 3;
-    tabuleiro[2][4] = 3; // Colocando navio horizontal no tabuleiro
-    tabuleiro[2][5] = 3;
-
-    tabuleiro[7][8] = 3;
-    tabuleiro[8][8] = 3; // Colocando navio vertical no tabuleiro
-    tabuleiro[6][8] = 3;
-
+int ExibeTabuleiro(char letras[20], int tabuleiro[10][10]){ // Imprime o tabuleiro 
+    system('cls'||'clear');
+    printf("-- B A T A L H A -- N A V A L --\n\n");
     int col = 1;
 
     for (int f = 0; f<10; f++){ // Imprimindo Linha de letras
         f == 0? printf("  "):0;
         printf("  %c", letras[f]); 
-        f == 9? printf("\n"):0;
+        f == 9? printf("\n\n"):0;
     }
 
     for(int i = 0; i<10; i++){ // Imprimindo tabuleiro com coluna de numeros
@@ -41,4 +25,44 @@ int main(){
         printf("\n");
     }
 
+
+}
+
+int LimpaMatriz(int tabuleiro[10][10]){ // Setando todos os valores da matriz para zero
+    for(int i = 0; i<10; i++){
+        for(int j = 0; j<10; j++){
+            tabuleiro[i][j] = 0; 
+
+        }
+    }
+}
+
+int GerarNavio(int tabuleiro[10][10], int posicao1, int posicao2){ // Gerando navios e fazendo validações
+
+    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'}; 
+
+    posicao1 = letras[posicao1]; //Tratando entradas...
+    posicao2 = posicao2 - 1;     //para que fiquem como no jogo real
+
+    if (posicao2 > 9){ // validando posição no tabuleiro
+        printf("Insira uma posição válida");
+        return 0;
+
+    }
+    if (tabuleiro[posicao1][posicao2] == 3){ // validando posição ocupada
+        printf("Já existe um navio nessa posição");
+        return 0;
+    }
+
+    tabuleiro[posicao2][posicao1] = 3; // gerando navio
+}
+
+int main(){
+
+    int tabuleiro[10][10]; // Setando matriz do tabuleiro
+    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'}; // Criando lista de letras
+    LimpaMatriz(tabuleiro);
+    GerarNavio(tabuleiro,'A',1);
+    ExibeTabuleiro(letras, tabuleiro);
+    
 }
